4310d01e14c351a34286a8881eb09eb6a5581e86
rpuiu <raz.puiu@gmail.com>
2019-10-08 20:33:27 +0300

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Razvan Puiu on 08/10/2019.
+//  Copyright © 2019 Razvan Puiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
